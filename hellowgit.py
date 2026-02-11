2f4cd9df97f8ec5edc60bed3c039e37bc370e58d
miguelS <msaldana.mdz@gmail.com>
2026-02-10 22:28:07 -0300

    v2.0

--- hellowgit.py
+++ hellowgit.py
@@ -1,2 +1,2 @@
-print ("Primera linea de codigo programada")
-print ("Segunda linea de codigo programada")
+print ("Primera linea de codigo programada - versión 2")
+print ("Segunda linea de codigo programada - version 2")
--- hellowgit2.py
+++ hellowgit2.py
@@ -1,1 +1,1 @@
-print("version 1.0")
+print("version 2.0")
